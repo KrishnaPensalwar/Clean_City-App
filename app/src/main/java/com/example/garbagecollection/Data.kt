8ddf7dde24bfe96daad02dfc02bd6deef9edb07d
Krishna Pensalwar <100767867+KrishnaPensalwar@users.noreply.github.com>
2024-08-10 00:07:21 +0530

    Update Data.kt

--- app/src/main/java/com/example/garbagecollection/Data.kt
+++ app/src/main/java/com/example/garbagecollection/Data.kt
@@ -1,7 +1,8 @@
 package com.example.garbagecollection
 
 data class Data(
-    var url:String,
-    var userid:String,
-    var status:String
+    val uri: String,
+    val userid: String,
+    val status: String,
+    val documentId: String 
 )
